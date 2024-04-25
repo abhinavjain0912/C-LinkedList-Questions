bcc14314477ac270419194850a69a0e9b07f5e30
Abhinav Jain <abhinavjain2233@gmail.com>
2024-04-25 19:54:13 +0530

    Reversing LinkedList using Recursion(Better Approach)

---
+++ Reversing LinkedList using Recursion(Better Approach).cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+  
+  public :
+  int data;
+  Node * next;
+  
+  Node(int data){
+    this->data = data;
+    next = NULL;
+  }
+}; 
+
+// Reversing LinkedList using recursion better Approach
+
+Node* reverseLinkedList(Node* head){
+  if(head==NULL || head->next == NULL){
+    return head;
+  }
+  Node* smallAns = reverseLinkedList(head->next);
+  // Head is Holding the Address of last Node of LinkedList
+  Node* tail = head->next;
+  tail->next = head;
+  head->next= NULL;
+  return smallAns;
+}
+
+void print(Node *head){
+  if(head==NULL){
+    return;
+  }
+  if(head->next==NULL){
+    cout<<head->data<<endl;
+  }
+  if(head->next!= NULL){
+    cout<<head->data<<endl;
+    print(head->next);
+  }
+}
+
+int main(){
+  Node n1(10);
+  Node * head = &n1;
+  Node n2(20);
+  n1.next = &n2;
+  Node n3(30);
+  n2.next= &n3;
+  
+  Node* newHead =reverseLinkedList(head);
+  print(newHead);
+}
